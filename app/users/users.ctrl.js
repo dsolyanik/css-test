1c992208ae25cb7cbb7fda6dd0bb987e2a19d5a8
dsolyanik <CvthnmIgbjyfv1>
2016-08-30 00:16:10 +0300

    add correct greeting

--- app/users/users.ctrl.js
+++ app/users/users.ctrl.js
@@ -7,35 +7,30 @@
 
     function Controller($scope, usersSvc, $timeout) {
 
-        $scope.userName = "est";
-
         usersSvc.getUsers()
             .success(function (data) {
                 $scope.users = data;
             });
 
         $scope.openDialog = function (id) {
-            $scope.id = id;
             $scope.isOpenDialog = true;
+            $scope.currentUser = _.find($scope.users, {id: id});
         };
 
         $scope.closeDialog = function () {
             $scope.isOpenDialog = false;
         };
 
-        $scope.confirm = function (id) {
-            usersSvc.setIsOnline(id);
-            
-            _.find($scope.users, {id: $scope.id}).online = true;
-            $scope.isOpenDialog = false;
-        };
-        
-        $scope.signIn = function () {
-            angular.element(document.querySelector('.greeting')).text("Have a nice day!");
-            angular.element(document.querySelector('#confirm')).text("");
-            $timeout($scope.closeDialog, 1500);
+        $scope.confirm = function () {
+            usersSvc.setIsOnline($scope.currentUser.id);
+            _.find($scope.users, {id: $scope.currentUser.id}).online = !$scope.currentUser.online;
+            $scope.greeting = true;
 
-        }
+            $timeout(function () {
+                $scope.closeDialog();
+                $scope.greeting = false;
+            }, 2000);
+        };
 
     }
 })();
